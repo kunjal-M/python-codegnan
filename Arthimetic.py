c7073c00df711bb3a180ec34365c140087436fe3
kunjal-M <kunjal.m@trominosoft.com>
2026-03-31 20:04:02 +0530

    Add files via upload

---
+++ Arthimetic.py
@@ -0,0 +1,25 @@
+Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> a = 40
+>>> b = 20
+>>> c = a+b
+>>> d = a-b
+>>> e = a*b
+>>> f = a/b
+>>> g = a%b
+>>> h = a//b
+>>> i = a**b
+>>> print("Addition of two numbers",c)
+Addition of two numbers 60
+>>> print("Subtraction of two numbers",d)
+Subtraction of two numbers 20
+>>> print("Multipication of two numbers",e)
+Multipication of two numbers 800
+>>> print("Division of two numbers",f)
+Division of two numbers 2.0
+>>> print("Modulus of two numbers",g)
+Modulus of two numbers 0
+>>> print("Floor division of two numbers",h)
+Floor division of two numbers 2
+>>> print("Exponent of two numbers",i)
+Exponent of two numbers 109951162777600000000000000000000

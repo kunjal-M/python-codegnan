237ce275b49221e2021c95ec28b3d1be08b5af17
kunjal-M <kunjal.m@trominosoft.com>
2026-04-01 20:35:10 +0530

    Add files via upload

---
+++ Relational.py
@@ -0,0 +1,20 @@
+Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> a = 10
+>>> b = 5
+>>> print("Value of a:", a)
+Value of a: 10
+>>> print("Value of b:", b)
+Value of b: 5
+>>> print("a == b:", a == b)
+a == b: False
+>>> print("a != b:", a != b)
+a != b: True
+>>> print("a > b:", a > b)
+a > b: True
+>>> print("a < b:", a < b)
+a < b: False
+>>> print("a >= b:", a >= b)
+a >= b: True
+>>> print("a <= b:", a <= b)
+a <= b: False

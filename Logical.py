667f5baa14232e658f52b92ad0e96a3411378c89
kunjal-M <kunjal.m@trominosoft.com>
2026-04-01 20:38:24 +0530

    Add files via upload

---
+++ Logical.py
@@ -0,0 +1,10 @@
+Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> a = 10
+>>> b = 5
+>>> print("(a > 5 and b < 10):", (a > 5 and b < 10))
+(a > 5 and b < 10): True
+>>> print("(a < 5 or b < 10):", (a < 5 or b < 10))
+(a < 5 or b < 10): True
+>>> print("not(a > 5):", not(a > 5))
+not(a > 5): False

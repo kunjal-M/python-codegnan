43f8c402ad1b979558085cf62913255a77582230
kunjal-M <kunjal.m@trominosoft.com>
2026-04-01 20:39:31 +0530

    Add files via upload

---
+++ mem&identity.py
@@ -0,0 +1,20 @@
+Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> # Membership Operators Example
+>>> fruits = ["apple", "banana", "cherry"]
+>>> print("fruits list:", fruits)
+fruits list: ['apple', 'banana', 'cherry']
+>>> print("'apple' in fruits:", 'apple' in fruits)
+'apple' in fruits: True
+>>> print("'mango' not in fruits:", 'mango' not in fruits)
+'mango' not in fruits: True
+>>> # Identity Operators Example
+>>> x = 10
+>>> y = 10
+>>> z = x
+>>> print("x is y:", x is y)
+x is y: True
+>>> print("x is z:", x is z)
+x is z: True
+>>> print("x is z:", x is z)
+x is z: True
